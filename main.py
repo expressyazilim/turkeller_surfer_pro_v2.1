92237ea85fccd367de15645274e9ef75826ea82d
expressyazilim <balcisalihhh@gmail.com>
2026-02-01 21:17:08 +0300

    Retrieve client credentials from Streamlit secrets
    Add client_id and client_secret retrieval from secrets

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 from utils.geo import zscore_to_heatmap, zscore_to_surface, plot_map
 from utils.storage import save_report, load_history
 
+client_id = st.secrets.get("client_id", "dummy")
+client_secret = st.secrets.get("client_secret", "dummy")
+
 st.set_page_config(page_title="Turkeller Surfer Pro", layout="wide")
 
 st.title("🌍 Turkeller Surfer Pro v2")
